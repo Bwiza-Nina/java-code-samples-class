481a6f34be448a228f03d5d82b39ffd734647bbc
Bwiza-Nina <ninamyles2020@gmail.com>
2022-05-07 13:08:41 +0200

    Added the actionlistener function

--- javaGui/src/com/thread/UserForm.java
+++ javaGui/src/com/thread/UserForm.java
@@ -2,6 +2,7 @@ package com.thread;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
@@ -25,6 +26,7 @@ class MyForm extends JFrame implements ActionListener{
     private JTextArea tadd;
     private JCheckBox term;
     private JButton submit;
+    private JButton reset;
     private JTextArea tout;
     private JLabel res;
     private JTextArea resadd;
@@ -60,12 +62,68 @@ class MyForm extends JFrame implements ActionListener{
         setBounds(300, 90, 900, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
+
+        c = getContentPane();
+        c.setLayout(null);
+
+        title = new JLabel("Registration Form");
+        title.setFont(new Font("Arial", Font.PLAIN, 30));
+        title.setSize(300, 30);
+        title.setLocation(300, 30);
+        c.add(title);
+
+        name = new JLabel("Name");
+        name.setFont(new Font("Arial", Font.PLAIN, 20));
+        name.setSize(100, 20);
+        name.setLocation(100, 100);
+        c.add(name);
+
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == submit){
+            if(term.isSelected()){
+                String data1;
+                String data = "Name: "+tname.getText() + "\n" + "Mobile: " + tmno.getText() + "\n";
+
+                if(male.isSelected()){
+                    data1 = "Gender: Male" + "\n";
+                }else{
+                    data1 = "Gender: Female" + "\n";
+                }
+                String data2 = "DOB: "+(String)date.getSelectedItem() + "/" + (String)month.getSelectedItem() + "/" + (String)year.getSelectedItem() + "\n";
+                String data3 = "Address: " + tadd.getText();
+                tout.setText(data + data1 + data2 + data3);
+                tout.setEditable(false);
+                res.setText("Registration Successful");
+            }
+            else{
+                tout.setText("");
+                resadd.setText("");
+                res.setText("Please accept the terms and conditions..");
+            }
+        }
+        else if(e.getSource() == reset){
+            String def = "";
+            tname.setText(def);
+            tadd.setText(def);
+            tmno.setText(def);
+            res.setText(def);
+            tout.setText(def);
+            term.setSelected(false);
+            date.setSelectedIndex(0);
+            month.setSelectedIndex(0);
+            year.setSelectedIndex(0);
+            resadd.setText(def);
+        }
     }
 }
 
 public class UserForm {
 
-    public static main(String []args ) throws IOException{
+    public static void main(String []args ) throws IOException{
         MyForm form = new MyForm();
     }
 }
