36f0698f000d6efb81877ffc2e4dc8d66643a752
Bwiza-Nina <ninamyles2020@gmail.com>
2022-05-08 15:54:16 +0200

    Added changes

--- javaGui/src/com/thread/UserForm.java
+++ javaGui/src/com/thread/UserForm.java
@@ -168,6 +168,37 @@ class MyForm extends JFrame implements ActionListener{
         submit.setSize(100, 20);
         submit.setLocation(150, 450);
         submit.addActionListener(this);
+        c.add(submit);
+
+        reset = new JButton("Reset");
+        reset.setFont(new Font("Arial", Font.PLAIN, 15));
+        reset.setSize(100, 20);
+        reset.setLocation(270, 450);
+        reset.addActionListener(this);
+        c.add(reset);
+
+        tout = new JTextArea();
+        tout.setFont(new Font("Arial", Font.PLAIN, 15));
+        tout.setSize(300, 400);
+        tout.setLocation(500, 100);
+        tout.setLineWrap(true);
+        tout.setEditable(false);
+        c.add(tout);
+
+        res = new JLabel("");
+        res.setFont(new Font("Arial", Font.PLAIN, 20));
+        res.setSize(500, 25);
+        res.setLocation(100, 500);
+        c.add(res);
+
+        resadd = new JTextArea();
+        resadd.setFont(new Font("Arial", Font.PLAIN, 15));
+        resadd.setSize(200, 75);
+        resadd.setLocation(580, 175);
+        resadd.setLineWrap(true);
+        c.add(resadd);
+
+        setVisible(true);
 
     }
 
